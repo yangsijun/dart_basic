88b791bcd3af5a613945f5ea6189b48cd08d392b
yangsijun <sijun0905@gmail.com>
2022-07-24 01:30:17 +0900

    1-5. Self Introduction

---
+++ 1. Data Type/1-5. Self Introduction1.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'Peter';
+  int age = 21;
+  String hobby = 'playing soccer';
+
+	String printString = 'Hi! My name is $name.\nI\'m $age years old and I like $hobby.';
+
+	print(printString);
+}
