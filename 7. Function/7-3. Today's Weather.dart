772b98c937c373da8914a6e790c25edfed78b1a6
yangsijun <sijun0905@gmail.com>
2022-07-24 02:13:32 +0900

    7-3. Today's Weather

---
+++ 7. Function/7-3. Today's Weather.dart
@@ -0,0 +1,9 @@
+void main() {
+  countries('Italy', 'Spain', 'France');
+}
+
+void countries (String a, String b, String c) {
+  print("The weather in $a is sunny.");
+  print("The weather in $b is rainy.");
+  print("The weather in $c is rainy.");
+}
