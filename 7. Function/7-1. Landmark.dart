e80a2b8b99ef8d35c523ed1179deda1ece0c9e28
yangsijun <sijun0905@gmail.com>
2022-07-24 02:12:04 +0900

    7-1. Landmark

---
+++ 7. Function/7-1. Landmark.dart
@@ -0,0 +1,9 @@
+void main() {
+  cities("New York", "Sydney", "Tanzania");
+}
+
+void cities(String city1, String city2, String city3) {
+  print("Statue of Liberty is $city1");
+  print("Opera House is $city2");
+  print("Mount Killmanjaro is $city3");
+}
