e30d6d2c7c89f6ed41d224589709c72242557707
yangsijun <sijun0905@gmail.com>
2022-07-24 01:40:17 +0900

    2-1. Update Number

---
+++ 2. Operator/2-1. Update Number.dart
@@ -0,0 +1,10 @@
+void main() {
+  var num = 2;
+  var result = 38;
+
+	/* write your code here */
+  result *= num; // result == 76
+  result += --num; // num == 1, result == 77
+
+	print(result);
+}
